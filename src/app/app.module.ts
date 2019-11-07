44a9a72a95d4cb89d02cce1d66566337d094c7fa
harideepreddy <harideepreddy2@gmail.com>
2019-11-07 21:54:13 +0530

    2 files modified, 2 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { EmployeeComponent } from './employee.component';
+import { EmployeeComponent} from './employee.component';
 
 @NgModule({ 
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,EmployeeComponent ],
+  declarations: [ AppComponent, HelloComponent, EmployeeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/employee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-employee',
+  templateUrl: ''
+})
+export class EmployeeComponent {
+  firstname:string = "harideep";
+  lastname:string = "Reddy";
+  gender:string = "Male";
+  age:number = 24;
+}
